13e89532c0c94b99b5b6390f659e9eaf1695ed58
lac66 <lacarpenter66@gmail.com>
2021-10-14 21:43:34 -0400

    first commit

---
+++ MyKitchen/MyKitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyKitchenApp.swift
+//  MyKitchen
+//
+//  Created by Levi Carpenter on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct MyKitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

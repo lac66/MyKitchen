6a39fed35ac38389b8738b12a6bbd3b59a33fc87
Erick Garcia-Lopez <egarci26@uncc.edu>
2021-10-22 17:36:06 -0400

    Created my new branch from master and created the MealViewer View

---
+++ MyKitchen/Views/MealViewer.swift
@@ -0,0 +1,20 @@
+//
+//  MealViewer.swift
+//  MyKitchen
+//
+//  Created by Erick Garcia-Lopez on 10/22/21.
+//
+
+import SwiftUI
+
+struct MealViewer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MealViewer_Previews: PreviewProvider {
+    static var previews: some View {
+        MealViewer()
+    }
+}

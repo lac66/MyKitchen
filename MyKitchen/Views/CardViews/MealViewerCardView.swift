cfd4ff26b2bbab5a9f286d1dbcd2226dbfad4b03
Erick Garcia-Lopez <egarci26@uncc.edu>
2021-10-22 20:13:34 -0400

    Created Meal Viewer Card. Still needs additional fixes.

---
+++ MyKitchen/Views/CardViews/MealViewerCardView.swift
@@ -0,0 +1,63 @@
+//
+//  MealViewerCard.swift
+//  MyKitchen
+//
+//  Created by Erick Garcia-Lopez on 10/22/21.
+//
+
+import SwiftUI
+
+struct MealViewerCardView: View {
+    let recipe: Recipe
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Super very long recipe name to see overflow")
+                    .font(.system(size: 24, design: .default))
+                    .foregroundColor(Color("OxfordBlue"))
+                    .padding(.bottom, 1)
+                Spacer()
+                ZStack {
+                    Rectangle()
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(Color("Camel"))
+                        .cornerRadius(4)
+                    
+                    Image(systemName: "heart.fill") //change to a setting icon
+                }
+            }
+            .padding(.horizontal, 10.0)
+            Image("food")
+                .resizable()
+                .frame(width: 350, height: 140)
+                .cornerRadius(6)
+                .padding(.bottom, 5.0)
+                .padding(/*@START_MENU_TOKEN@*/.top, -10.0/*@END_MENU_TOKEN@*/)
+            
+            //            VStack(alignment: .leading) {
+            //                Text(recipe.name)
+            //                    .font(.system(size: 24, weight: .bold, design: .default))
+            //                    .padding(.bottom, 1)
+            //                Text(recipe.cookTime)
+            //                    .font(.system(size: 16, weight: .regular, design: .default))
+            //                Text(recipe.difficulty)
+            //                    .font(.system(size: 16, weight: .regular, design: .default))
+            //            }
+            
+
+        }
+        .frame(width: 400, height: 175)
+        .background(Color("AirBlue"))
+        .foregroundColor(Color("MintCream"))
+        .cornerRadius(15)
+    }
+}
+
+struct MealViewerCardView_Previews: PreviewProvider {
+    static var recipe = Recipe.data[0]
+    static var previews: some View {
+        MealViewerCardView(recipe: recipe)
+            .background(Color.white)
+            .previewLayout(.fixed(width: 400, height: 175))
+    }
+}
--- MyKitchen/Views/MealViewer.swift
+++ MyKitchen/Views/MealViewer.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct MealViewer: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    let recipes: [Recipe]
+    
+    init(recipeList: [Recipe]) {
+        recipes = recipeList
+        
+        coloredNavAppearance.backgroundColor = UIColor(named: "OxfordBlue")
+        
+        UINavigationBar.appearance().standardAppearance = coloredNavAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredNavAppearance
     }
+    
+    var body: some View {
+        // entire page should be UICollectionView where you can drag from top HStack to bottom V Stack
+        VStack {
+            Text("Meal Viewer")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            ScrollView(.horizontal, showsIndicators: true){
+                HStack{
+                    ForEach(recipes, id: \.id) { recipe in
+                        MealViewerCardView(recipe: recipe)
+                    }
+                }
+                
+            }
+            Spacer()
+
+        }
+        
+    }
+    
 }
 
+
 struct MealViewer_Previews: PreviewProvider {
+    static var recipes = Recipe.data
     static var previews: some View {
-        MealViewer()
+        MealViewer(recipeList: recipes)
     }
 }

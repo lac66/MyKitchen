bec88a306d2f4f147c6ea139d3d3898ba1b1423c
lac66 <80915025+lac66@users.noreply.github.com>
2021-11-03 15:30:44 -0400

    Add files via upload
    I have no idea why i need to add this back. I hate xcode

---
+++ MyKitchen/Models/modelData.swift
@@ -0,0 +1,29 @@
+//
+//  modelData.swift
+//  MyKitchen
+//
+//  Created by Noah Gillespie on 10/21/21.
+//
+//
+//import Foundation
+//import SwiftUI
+//
+//struct modelData: Identifiable {
+//    var id = UUID()
+//    let name: String
+//    var type: String
+//    var qty: String?
+//    var img: UIImage?
+//    var items: [modelData]?
+//}
+//
+//    let groceryList = [
+//        Ingredient(name: "Milk", type: "Dairy", qty: "1 gallon"),
+//        Ingredient(name: "Cheese", type: "Dairy", qty: "16 oz"),
+//        Ingredient(name: "Lettuce", type: "Vegetable", qty: "1 unit"),
+//        Ingredient(name: "Pasta", type: "Grain", qty: "8 oz"),
+//        Ingredient(name: "Apple", type: "Fruit", qty: "2 units"),
+//        Ingredient(name: "Ground Beef", type: "Protein", qty: "2 lbs")
+//    ]
+//    
+//    

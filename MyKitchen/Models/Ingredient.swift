30c49b58d9939203d8d49be524c362c019fa327d
lac66 <lacarpenter66@gmail.com>
2021-11-03 17:39:49 -0400

    Test after issues

--- MyKitchen/Models/Ingredient.swift
+++ MyKitchen/Models/Ingredient.swift
@@ -12,7 +12,7 @@ struct Ingredient: Identifiable {
     var id = UUID()
     let name: String
     var type: String?
-    var qty: String?
+    var qty: Quantity?
     var img: Image?
     var items: [Ingredient]?
 }
@@ -21,12 +21,12 @@ struct Ingredient: Identifiable {
 extension Ingredient {
     static var data: [Ingredient] {
         [
-            Ingredient(name: "Milk", type: "Dairy", qty: "1 gallon", img: Image("milk")),
-            Ingredient(name: "Cheese", type: "Dairy", qty: "16 oz", img: Image("cheese")),
-            Ingredient(name: "Lettuce", type: "Vegetable", qty: "1 unit", img: Image("lettuce")),
-            Ingredient(name: "Pasta", type: "Grain", qty: "8 oz", img: Image("pasta")),
-            Ingredient(name: "Apple", type: "Fruit", qty: "2 units", img: Image("apple")),
-            Ingredient(name: "Ground Beef", type: "Protein", qty: "2 lbs", img: Image("gbeef"))
+            Ingredient(name: "Milk", type: "Dairy", qty: Quantity(amt: 1, unit: Unit.gal), img: Image("milk")),
+            Ingredient(name: "Cheese", type: "Dairy", qty: Quantity(amt: 16, unit: Unit.oz), img: Image("milk")),
+            Ingredient(name: "Lettuce", type: "Vegetable", qty: Quantity(amt: 1, unit: Unit.unit), img: Image("milk")),
+            Ingredient(name: "Pasta", type: "Grain", qty: Quantity(amt: 8, unit: Unit.oz), img: Image("milk")),
+            Ingredient(name: "Apple", type: "Fruit", qty: Quantity(amt: 2, unit: Unit.unit), img: Image("milk")),
+            Ingredient(name: "Ground Beef", type: "Protein", qty: Quantity(amt: 2, unit: Unit.lb), img: Image("milk"))
         ]
         
         
